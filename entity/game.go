59613db15bca208bdcf4caa8bb75e919fb9cbeb2
sadeg <Sadeghkhoshbayan1@gmail.com>
2024-02-08 19:44:52 +0330

    add start time to game entity

--- entity/game.go
+++ entity/game.go
@@ -1,11 +1,14 @@
 package entity
 
+import "time"
+
 type Game struct {
 	ID          uint
 	CategoryID  uint
 	QuestionIDs []uint
 	PlayerIDs   []uint
 	WinnerID    uint
+	StartTime   time.Time
 }
 
 type Player struct {
